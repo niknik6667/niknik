4ea0f46f4a29b396584b3493a4328cefa5e3c8f4
unknown <niknik220306@gmail.com>
2024-10-22 01:45:47 +0500

    Первый коммит

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Пример: добавление действия при клике на кнопку "Вход"
+    document.querySelector('.nav-button').addEventListener('click', function() {
+        alert('Функция входа в разработке');
+    });
+});
